ca41cdc5ebf9a2e0cacf47831b2332f8ef605f84
David <David@DESKTOP-496B992>
2017-12-01 08:35:26 +0100

    Creada la ventana principal para la creacion de alumnos, creacion de las dos clases "Personas" y "Tiempo" añadido el nombre a la tabla

---
+++ src/prueba_git/AplicacionAlumnos.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba_git;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author DavidyManolo
+ */
+public class AplicacionAlumnos extends Application
+{
+    
+    private Stage primaryStage;
+    private AnchorPane rootLayout;
+    
+     
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception 
+    {
+         launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception 
+    {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Asistencia Alumnos");
+        
+        initRootLayout();
+        
+    }
+    
+    public void initRootLayout() 
+    {
+        try {
+             // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(AplicacionAlumnos.class.getResource("VentaPrincipal.fxml"));
+            rootLayout = (AnchorPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+
+            VentaPrincipalController controlador=loader.getController();
+            controlador.setMainApp(this);
+            
+            
+            primaryStage.show();
+            
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        
+    }
+    
+ 
+    
+}
---
+++ src/prueba_git/Persona.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba_git;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author DavidyManolo
+ */
+public class Persona 
+{
+    private String nombre_completo;
+    
+    private ArrayList<String>Cursos = new ArrayList<>();
+    
+    private ArrayList<String>Asignaturas = new ArrayList<>();
+    
+    private ArrayList<Object>Asistencia = new ArrayList<>();
+
+    public ArrayList<Object> getAsistencia() {
+        return Asistencia;
+    }
+
+    public void setAsistencia(ArrayList<Object> Asistencia) {
+        this.Asistencia = Asistencia;
+    }
+
+    public ArrayList<Tiempo> getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(ArrayList<Tiempo> tiempo) {
+        this.tiempo = tiempo;
+    }
+    
+    private ArrayList<Tiempo>tiempo = new ArrayList<>();
+
+    public Persona(String nombre_completo, ArrayList<String>Cursos, ArrayList<String>Asignaturas) 
+    {
+        this.nombre_completo = nombre_completo;
+        this.Cursos = Cursos;
+        this.Asignaturas = Asignaturas;
+    }
+    
+    public Persona()
+    {
+        nombre_completo = " ";
+    }
+
+    public String getNombre_completo() 
+    {
+        return nombre_completo;
+    }
+
+    public void setNombre_completo(String nombre_completo) 
+    {
+        this.nombre_completo = nombre_completo;
+    }
+
+    public ArrayList<String> getCursos() 
+    {
+        return Cursos;
+    }
+
+    public void setCursos(ArrayList<String> Cursos) 
+    {
+        this.Cursos = Cursos;
+    }
+
+    public ArrayList<String> getAsignaturas() 
+    {
+        return Asignaturas;
+    }
+
+    public void setAsignaturas(ArrayList<String> Asignaturas) 
+    {
+        this.Asignaturas = Asignaturas;
+    }
+    
+    public ArrayList<Persona> Obtenerpersonas(String curso, String asignatura, ArrayList<Persona> total)
+    {
+       ArrayList<Persona> personas = new ArrayList<>();
+       int cierto = 0;
+        for (int i = 0; i < total.size(); i++) 
+        {
+            for (int j = 0; j < total.get(i).getCursos().size(); j++) 
+            {
+                if (total.get(i).getCursos().get(j).equals(curso)) 
+                {
+                    cierto++;
+                }
+            }
+            for (int j = 0; j < total.get(i).getAsignaturas().size(); j++) 
+            {
+                if (total.get(i).getAsignaturas().get(j).equals(asignatura)) 
+                {
+                    cierto++;
+                }
+            }
+ 
+            if (cierto == 2) 
+            {
+                personas.add(total.get(i));
+            }
+            
+            cierto = 0;    
+        }
+        
+        return personas;
+    }
+ 
+}
